d9dc12253ebea1b0dea3ea4039876a348a58ffcf
mattgibson18@outlook.com <mattgibson18@outlook.com>
2024-03-28 11:25:43 +0000

    clear screen implemented

--- main.ts
+++ main.ts
@@ -10,22 +10,40 @@ const outputScreen = document.querySelector<HTMLElement>(`.calculator__output`);
 const previousTotalOutput =
   document.querySelector<HTMLElement>(`#previous-operand`);
 
+const currentTotalOutput =
+  document.querySelector<HTMLElement>(`#current-operand`);
+
 let currentInput = "";
 
-if (!inputButtons || !outputScreen || !previousTotalOutput) {
+if (
+  !inputButtons ||
+  !outputScreen ||
+  !previousTotalOutput ||
+  !currentTotalOutput
+) {
   throw new Error(`issue with the query selectors`);
 }
 
 // try to get the previous operator to show the values that are being pressed
 const outputScreenUpdate = (value: string) => {
   currentInput += value;
-  previousTotalOutput.innerText = currentInput;
+  currentTotalOutput.innerText = currentInput;
+};
+
+const clear = () => {
+  previousTotalOutput.innerText = "";
+  currentTotalOutput.innerText = "";
+  currentInput = "";
 };
 
 inputButtons.forEach((button) => {
   button.addEventListener(`click`, () => {
     const value = button.dataset.value;
-    console.log(value);
-    outputScreenUpdate(`${value}`);
+
+    if (value == "ac") {
+      clear();
+    } else {
+      outputScreenUpdate(`${value}`);
+    }
   });
 });
