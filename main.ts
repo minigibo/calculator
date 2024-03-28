b1d334384e56a02d91b4d68aeeb88a227976642e
mattgibson18@outlook.com <mattgibson18@outlook.com>
2024-03-28 10:21:53 +0000

    initial TS getting button inputs with eventListener and forEach

--- main.ts
+++ main.ts
@@ -1,4 +1,24 @@
 import "./style.scss";
 import { themeChange } from "./theme";
-
 themeChange();
+
+const inputButtons =
+  document.querySelectorAll<HTMLButtonElement>(`.calculator__button`);
+
+const outputScreen = document.querySelector<HTMLElement>(`.calculator__output`);
+
+const previousTotalOutput =
+  document.querySelector<HTMLElement>(`#previous-operand`);
+
+const runningTotal: string[] = [];
+
+if (!inputButtons || !outputScreen || !previousTotalOutput) {
+  throw new Error(`issue with the query selectors`);
+}
+
+inputButtons.forEach((button) => {
+  button.addEventListener(`click`, () => {
+    const value = button.dataset.value;
+    console.log(value);
+  });
+});
