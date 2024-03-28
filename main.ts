6ca64c2e30a96249cb620ff7807f642a13b89ace
mattgibson18@outlook.com <mattgibson18@outlook.com>
2024-03-28 10:41:41 +0000

    TypeScript update so buttons pressed feed into output screen

--- main.ts
+++ main.ts
@@ -10,15 +10,22 @@ const outputScreen = document.querySelector<HTMLElement>(`.calculator__output`);
 const previousTotalOutput =
   document.querySelector<HTMLElement>(`#previous-operand`);
 
-const runningTotal: string[] = [];
+let currentInput = "";
 
 if (!inputButtons || !outputScreen || !previousTotalOutput) {
   throw new Error(`issue with the query selectors`);
 }
 
+// try to get the previous operator to show the values that are being pressed
+const outputScreenUpdate = (value: string) => {
+  currentInput += value;
+  previousTotalOutput.innerText = currentInput;
+};
+
 inputButtons.forEach((button) => {
   button.addEventListener(`click`, () => {
     const value = button.dataset.value;
     console.log(value);
+    outputScreenUpdate(`${value}`);
   });
 });
