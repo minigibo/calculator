e8862ce12f826ca75e3d50316cdc17bd4dbf7078
mattgibson18@outlook.com <mattgibson18@outlook.com>
2024-03-28 15:30:13 +0000

    completion of calculation operation

--- main.ts
+++ main.ts
@@ -25,8 +25,18 @@ if (
   throw new Error(`issue with the query selectors`);
 }
 
-// try to get the previous operator to show the values that are being pressed
+// try to get the previous operator to show the values that are being pressed. Also preventing a decimal place at the start or more than once.
 const outputScreenUpdate = (value: string) => {
+  const decimalPointCheck = currentInput.includes(".");
+  if (currentInput === "" && value === "%") {
+    return;
+  }
+  if (currentInput === "" && value === ".") {
+    return;
+  }
+  if (decimalPointCheck && value === ".") {
+    return;
+  }
   currentInput += value;
   const formattedInput = addCommas(currentInput);
   currentTotalOutput.innerText = formattedInput;
@@ -56,7 +66,10 @@ const backSpace = () => {
 };
 
 // function to change sign when +/- pressed
-const signChange = () => {
+const signChange = (value: string) => {
+  if (currentInput === "" && value === "+-") {
+    return;
+  }
   const currentText = currentTotalOutput.innerText.replace(/,/g, "");
   const newValue = parseFloat(currentText) * -1;
   currentInput = newValue.toString();
@@ -74,14 +87,61 @@ const moveValueToPrevious = (operator: string) => {
   }
 };
 
-//
+// calculation function using switch case to determine which operation to carry out
+const calculationOperation = () => {
+  const previousValue = parseFloat(
+    previousTotalOutput.innerText.replace(/,/g, "")
+  );
+  const currentValue = parseFloat(
+    currentTotalOutput.innerText.replace(/,/g, "")
+  );
+  const operation = previousTotalOutput.innerText.split(" ")[1];
+  let result: number;
+  switch (operation) {
+    case "+":
+      result = previousValue + currentValue;
+      break;
+    case "-":
+      result = previousValue - currentValue;
+      break;
+    case "*":
+      result = previousValue * currentValue;
+      break;
+    case "/":
+      result = previousValue / currentValue;
+      break;
+    default:
+      throw new Error("Invalid operation");
+  }
+  currentTotalOutput.innerText = addCommas(result.toString());
+  previousTotalOutput.innerText = "";
+};
+
+// function for if % button is pressed effectively divdes the current output by 100 first checking if there is a value in there
+const percentage = (value: string) => {
+  if (currentInput === "" && value === "%") {
+    return;
+  }
+  const currentValue = currentTotalOutput.innerText.replace(/,/g, "");
+  if (previousTotalOutput.innerText) {
+    const previousValue = parseFloat(
+      previousTotalOutput.innerText.split(" ")[0].replace(/,/g, "")
+    );
+    const percentageValue = (parseFloat(currentValue) / previousValue) * 100;
+    currentTotalOutput.innerText = percentageValue.toFixed(2).toString();
+  } else {
+    const percentageValue = parseFloat(currentValue) / 100;
+    currentTotalOutput.innerText = percentageValue.toFixed(2).toString();
+  }
+  currentTotalOutput.innerText = addCommas(currentTotalOutput.innerText);
+};
 
 // running for each loop that using an event listenener to run each time one of the calculator buttons are pressed
 inputButtons.forEach((button) => {
   button.addEventListener(`click`, () => {
     const value = button.dataset.value;
 
-    if (value == "ac") {
+    if (value === "ac") {
       clear();
     } else if (
       value === "/" ||
@@ -89,11 +149,18 @@ inputButtons.forEach((button) => {
       value === "-" ||
       value === "+"
     ) {
+      if (previousTotalOutput.innerText && currentTotalOutput.innerText) {
+        calculationOperation();
+      }
       moveValueToPrevious(value);
+    } else if (value === "%") {
+      percentage(`${value}`);
     } else if (value === "+-") {
-      signChange();
+      signChange(`${value}`);
     } else if (value === "del") {
       backSpace();
+    } else if (value === "=") {
+      calculationOperation();
     } else {
       outputScreenUpdate(`${value}`);
     }
